5642f15c196c8841ce93b456d0a13adb50529b98
consuelita <adelita@operatorfoundation.org>
2026-04-29 14:36:08 -0500

    AudioCoder v1.3.5

--- SignalBridge/build.gradle.kts
+++ SignalBridge/build.gradle.kts
@@ -84,7 +84,7 @@ afterEvaluate {
 }
 
 dependencies {
-    implementation("com.github.OperatorFoundation:AudioCoderAndroid:v1.3.4")
+    implementation("com.github.OperatorFoundation:AudioCoderAndroid:v1.3.5")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
